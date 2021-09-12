706f77733a9e31f5d33ff5a4141df877da944d0f
Serge Za <zaserge@gmail.com>
2021-09-12 14:41:43 +0300

    Fix Codacy 2

--- getlist.js
+++ getlist.js
@@ -1,25 +1,4 @@
 
-var url = new URL(window.location.href);
-const order = url.searchParams.get('order') ?? 'scene';
-
-const httpRequest = new XMLHttpRequest();
-var elem = document.getElementById('getlist');
-
-httpRequest.onprogress = function (event) {
-    elem.innerHTML = event.currentTarget.response;
-}
-
-httpRequest.onload = function () {
-    if (httpRequest.readyState === httpRequest.DONE && httpRequest.status === 200) {
-        document.getElementById('progress').style.display = 'none';
-        document.title = document.getElementById('title').textContent;
-        setEventHandlers();
-    }
-}
-
-httpRequest.open('GET', './getlist.php?order=' + order, true);
-httpRequest.send(null);
-
 function setEventHandlers() {
     var items = elem.getElementsByClassName("toggleitem");
 
@@ -36,3 +15,27 @@ function setEventHandlers() {
         });
     }
 }
+
+
+
+var url = new URL(window.location.href);
+const order = url.searchParams.get("order") ?? "scene";
+
+const httpRequest = new XMLHttpRequest();
+var elem = document.getElementById("getlist");
+
+httpRequest.onprogress = function (event) {
+    const response = event.currentTarget.response;
+    elem.innerHTML = response;
+};
+
+httpRequest.onload = function () {
+    if (httpRequest.readyState === httpRequest.DONE && httpRequest.status === 200) {
+        document.getElementById("progress").style.display = "none";
+        document.title = document.getElementById("title").textContent;
+        setEventHandlers();
+    }
+};
+
+httpRequest.open("GET", "./getlist.php?order=" + order, true);
+httpRequest.send(null);
